19fb222855a5d2e1198f369aa911e3143c142655
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-01-31 16:15:45 -0800

    Fixes to get images to build

--- Core/shared/admin/build-images.cmd
+++ Core/shared/admin/build-images.cmd
@@ -1,27 +1,27 @@
 @echo off
-pushd
 
-rem ==================================================
 SET CONTAINER_ROOT_PATH=d:\em\core
 SET IMAGE_OWNER=rodneywimberly
 
-rem ==================================================
-echo "Building Docker Images"
+echo Building Docker Images
 
-cd %CONTAINER_ROOT_PATH%\consul
-echo " --> Building %IMAGE_OWNER%/consul image"
+pushd %CONTAINER_ROOT_PATH%\consul
+echo  --- Building %IMAGE_OWNER%/consul image
 docker build -t %IMAGE_OWNER%/consul .
-
-cd %CONTAINER_ROOT_PATH%\dns
-echo " --> Building %IMAGE_OWNER%/dns image"
-docker build -t %IMAGE_OWNER%/dns .
-
-cd %CONTAINER_ROOT_PATH%\portal
-echo " --> Building %IMAGE_OWNER%/portal image"
-docker build -t %IMAGE_OWNER%/portal .
-
-cd %CONTAINER_ROOT_PATH%\vault
-echo " --> Building %IMAGE_OWNER%/vault image"
-docker build -t %IMAGE_OWNER%/vault .
-
 popd
+
+pushd %CONTAINER_ROOT_PATH%\dns
+echo  --- Building %IMAGE_OWNER%/dns image
+docker build -t %IMAGE_OWNER%/dns .
+popd
+
+pushd %CONTAINER_ROOT_PATH%\portal
+echo  --- Building %IMAGE_OWNER%/portal image
+docker build -t %IMAGE_OWNER%/portal .
+popd
+
+pushd %CONTAINER_ROOT_PATH%\vault
+echo  --- Building %IMAGE_OWNER%/vault image
+docker build -t %IMAGE_OWNER%/vault .
+popd
+
