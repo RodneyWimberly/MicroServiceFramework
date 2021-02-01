d75a433902e490e586f6c8c12c9a0cb130569fe3
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-02-01 03:51:03 -0800

    Repaired COnsul startup arguments

--- Core/shared/admin/manager.cmd
+++ Core/shared/admin/manager.cmd
@@ -1,3 +1,3 @@
-start /B putty.exe ip172-18-0-17-c0ble8k34gag00aq8sug@direct.labs.play-with-docker.com
+start /B putty.exe ip172-18-0-46-c0buhhhlo55000fjigbg@direct.labs.play-with-docker.com
 rem start /B putty.exe ip172-18-0-57-c0aj9g36hnp000fak9p0@direct.labs.play-with-docker.com
 rem start /B putty.exe ip172-18-0-31-c0aj9g36hnp000fak9p0@direct.labs.play-with-docker.com
--- Core/shared/admin/worker.cmd
+++ Core/shared/admin/worker.cmd
@@ -1,2 +1,2 @@
 start /B putty ip172-18-0-43-c0b4q1b6hnp000erano0@direct.labs.play-with-docker.com
-rem start /B putty ip172-18-0-55-c0aj9g36hnp000fak9p0@direct.labs.play-with-docker.com
+
