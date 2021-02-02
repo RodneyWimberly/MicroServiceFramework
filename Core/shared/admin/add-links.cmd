9b443676733ad6132b9c8aa7d05ef66eb94b1d33
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-02-02 15:49:22 -0800

    Fixed consul publised port to be host not ingress

--- Core/shared/admin/add-links.cmd
+++ Core/shared/admin/add-links.cmd
@@ -1,5 +1,10 @@
 @echo off
-SET CORE_DIR=d:\em\core
+SET MSF_DIR=d:\em
+SET CORE_DIR=%MSF_DIR%\core
+
+echo  --- Making links for Core stack
+mklink /H %CORE_DIR%\.gitignore d:\em\.gitignore
+mklink /H %CORE_DIR%\.dockerignore d:\em\.dockerignore
 
 echo  --- Making links for Consul
 mklink /H %CORE_DIR%\consul\consul-agent-ca.pem %CORE_DIR%\shared\certs\consul-agent-ca.pem
@@ -7,17 +12,17 @@ mklink /H %CORE_DIR%\consul\docker-client-consul-0.pem %CORE_DIR%\shared\certs\d
 mklink /H %CORE_DIR%\consul\docker-client-consul-0-key.pem %CORE_DIR%\shared\certs\docker-client-consul-0-key.pem
 mklink /H %CORE_DIR%\consul\docker-server-consul-0.pem %CORE_DIR%\shared\certs\docker-server-consul-0.pem
 mklink /H %CORE_DIR%\consul\docker-server-consul-0-key.pem %CORE_DIR%\shared\certs\docker-server-consul-0-key.pem
-mklink /H %CORE_DIR%\consul\.dockerignore d:\em\.dockerignore
+mklink /H %CORE_DIR%\consul\.dockerignore %MSF_DIR%\.dockerignore
 mklink /H %CORE_DIR%\consul\core.env %CORE_DIR%\shared\scripts\core.env
 mklink /H %CORE_DIR%\consul\common-functions.sh %CORE_DIR%\shared\scripts\common-functions.sh
 
 echo  --- Making links for DNS
-mklink /H %CORE_DIR%\dns\.dockerignore d:\em\.dockerignore
+mklink /H %CORE_DIR%\dns\.dockerignore %MSF_DIR%\.dockerignore
 mklink /H %CORE_DIR%\dns\core.env %CORE_DIR%\shared\scripts\core.env
 mklink /H %CORE_DIR%\dns\common-functions.sh %CORE_DIR%\shared\scripts\common-functions.sh
 
 echo  --- Making links for Portal
-mklink /H %CORE_DIR%\portal\.dockerignore d:\em\.dockerignore
+mklink /H %CORE_DIR%\portal\.dockerignore %MSF_DIR%\.dockerignore
 mklink /H %CORE_DIR%\portal\core.env %CORE_DIR%\shared\scripts\core.env
 mklink /H %CORE_DIR%\portal\common-functions.sh %CORE_DIR%\shared\scripts\common-functions.sh
 
@@ -27,6 +32,6 @@ mklink /H %CORE_DIR%\vault\docker-client-consul-0.pem %CORE_DIR%\shared\certs\do
 mklink /H %CORE_DIR%\vault\docker-client-consul-0-key.pem %CORE_DIR%\shared\certs\docker-client-consul-0-key.pem
 mklink /H %CORE_DIR%\vault\docker-server-consul-0.pem %CORE_DIR%\shared\certs\docker-server-consul-0.pem
 mklink /H %CORE_DIR%\vault\docker-server-consul-0-key.pem %CORE_DIR%\shared\certs\docker-server-consul-0-key.pem
-mklink /H %CORE_DIR%\vault\.dockerignore d:\em\.dockerignore
+mklink /H %CORE_DIR%\vault\.dockerignore %MSF_DIR%\.dockerignore
 mklink /H %CORE_DIR%\vault\core.env %CORE_DIR%\shared\scripts\core.env
 mklink /H %CORE_DIR%\vault\common-functions.sh %CORE_DIR%\shared\scripts\common-functions.sh
