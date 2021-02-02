bae9b292f16f40c2bcdb834ca7d9c4d6cef9757d
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-02-01 20:46:03 -0800

    Vault command changes

--- Core/shared/admin/manager.cmd
+++ Core/shared/admin/manager.cmd
@@ -1,3 +1,3 @@
-start /B putty.exe ip172-18-0-46-c0buhhhlo55000fjigbg@direct.labs.play-with-docker.com
+start /B putty.exe ip172-18-0-11-c0cd5hr6hnp000c4spc0@direct.labs.play-with-docker.com
 rem start /B putty.exe ip172-18-0-57-c0aj9g36hnp000fak9p0@direct.labs.play-with-docker.com
 rem start /B putty.exe ip172-18-0-31-c0aj9g36hnp000fak9p0@direct.labs.play-with-docker.com
