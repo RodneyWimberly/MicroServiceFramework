d102d5db7596f3acc226340c937f79a9294a3a7b
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-02-04 22:32:30 -0800

    Modified Portal startup Modified portal config generation

--- Core/Portal/portal-entrypoint.sh
+++ Core/Portal/portal-entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-set +e
-
 # Make our stuff available
 cd $CORE_SCRIPT_DIR
 chmod 0755 common-functions.sh
@@ -9,25 +7,28 @@ chmod 0755 common-functions.sh
 
 add_path "${CORE_SCRIPT_DIR}"
 
-# get_consul_ip
-# export CONSUL_HTTP_ADDR=http://${CONSUL_IP}:8500
-
 # Get Docker/Node/Hosting information from the Docker API for use in configuration
 hosting_details
-# log "-----------------------------------------------------------"
-# log "- Portal Details"
-# log "-----------------------------------------------------------"
-# log_detail "Consul Address: ${CONSUL_IP}"
-# log_detail "Consul HTTP Address: ${CONSUL_HTTP_ADDR}"
+
+# Consul-template vars are encoded as ** not $ in our templates so that bash doesn't get confused.
+# First we use bash to replace all $vars with values
+# Second we replace all ** to $
+# Third we pass to consul-template
+log_detail "merging expanded variables and updating configuration based on Consul cluster deployment"
+
+cat /etc/templates/nginx.conf | envsubst | sed 's/**/$/g' > /etc/templates/nginx-template.conf
+run_consul_template /etc/templates/nginx-template.conf nginx.conf /etc/ngnix/conf.d/default.conf "consul lock -http-addr=http://consul.service.consul:8500 -name service/portal -shell=false reload nginx -s reload"
+
+cat /etc/templates/index.html | envsubst  | sed 's/**/$/g' > /etc/templates/index-template.html
+run_consul_template /etc/templates/index-template.html index.html /usr/share/nginx/html/index.html
+
+# Make a folder for ngnix.pid
+mkdir -p /run/nginx
+
+# Remove default configuration
+rm /usr/share/nginx/http-default_server.conf
 
 set +e
-
-log_detail "merging expanded variables and updating configuration based on Consul cluster deployment"
-# cat /etc/templates/nginx.conf | envsubst > /etc/templates/nginx-template.conf
-# cat /etc/templates/index.html | envsubst > /etc/templates/index-template.html
-run_consul_template /etc/templates/nginx.conf nginx.conf /etc/ngnix/conf.d/default.conf "consul lock -http-addr=http://consul.service.consul:8500 -name service/portal -shell=false reload nginx -s reload"
-run_consul_template /etc/templates/index.html index.html /usr/share/nginx/html/index.html
-cat /etc/ngnix/conf.d/default.conf
-mkdir -p /run/nginx
 nginx -g 'daemon off;'
+
 keep_container_alive
