032f1bdd4a84cfad3662fbbd5d56a53e62829125
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-02-04 23:18:48 -0800

    Nginx Changes

--- Core/Portal/portal-entrypoint.sh
+++ Core/Portal/portal-entrypoint.sh
@@ -16,17 +16,19 @@ hosting_details
 # Third we pass to consul-template
 log_detail "merging expanded variables and updating configuration based on Consul cluster deployment"
 
+# Remove default configuration
+if [[ -f /usr/share/nginx/http-default_server.conf ]]; then
+  rm -f /usr/share/nginx/http-default_server.conf
+fi
+
 cat /etc/templates/nginx.conf | envsubst | sed 's/**/$/g' > /etc/templates/nginx-template.conf
-run_consul_template /etc/templates/nginx-template.conf nginx.conf /etc/ngnix/conf.d/default.conf "consul lock -http-addr=http://consul.service.consul:8500 -name service/portal -shell=false reload nginx -s reload"
+run_consul_template /etc/templates/nginx-template.conf nginx.conf /etc/ngnix/http.d/default.conf "consul lock -http-addr=http://consul.service.consul:8500 -name service/portal -shell=false reload nginx -s reload"
 
 cat /etc/templates/index.html | envsubst  | sed 's/**/$/g' > /etc/templates/index-template.html
 run_consul_template /etc/templates/index-template.html index.html /usr/share/nginx/html/index.html
 
 # Make a folder for ngnix.pid
 mkdir -p /run/nginx
-
-# Remove default configuration
-rm /usr/share/nginx/http-default_server.conf
 
 set +e
 nginx -g 'daemon off;'
