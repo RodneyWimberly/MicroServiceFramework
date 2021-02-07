be370ccffc6ddecc29df405f2cc5c332ac211f56
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-02-07 06:34:45 -0800

    More dns regegistration testing

--- Core/Dns/dns-entrypoint.sh
+++ Core/Dns/dns-entrypoint.sh
@@ -7,7 +7,7 @@ add_path "${CORE_SCRIPT_DIR}"
 log_detail "merging expanded variables and updating configuration based on Consul cluster deployment"
 # get_consul_kv config/dns | envsubst > /etc/templates/1.consul-template.conf
 cat /etc/templates/1.consul.conf | envsubst > /etc/templates/1.consul-template.conf
-run_consul_template /etc/templates/1.consul-template.conf 1.consul.conf /etc/dnsmasq/1.consul.conf "consul lock -http-addr=http://consul.service.consul:8500 -name=service/dnsmasq -shell=false restart dnsmasq"
+run_consul_template /etc/templates/1.consul-template.conf 1.consul.conf /etc/dnsmasq/1.consul.conf "consul lock -http-addr=http://consul.service.consul:8500 -name=service/dnsmasq -shell=false restart killall dnsmasq"
 
 # Get Docker/Node/Hosting information from the Docker API for use in configuration
 hosting_details
