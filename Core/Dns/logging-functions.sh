7be81566bf6ea04951a0c2077ea0ba11b27b5a6b
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-02-09 12:28:07 -0800

    Updated logging-functions.shto correctly show colors

--- Core/Dns/logging-functions.sh
+++ Core/Dns/logging-functions.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 
 function log() {
-  log_raw "$GREEN[INF] $1$NC"
+  log_raw "$GREEN [INF] $1 $NC"
 }
 
 function log_header() {
   echo " "
-  log_raw "$PURPLE[HDR] -----------------------------------------------------------$NC"
-  log_raw "$PURPLE[HDR] - $1$NC"
-  log_raw "$PURPLE[HDR] -----------------------------------------------------------$NC"
+  log_raw "$PURPLE [HDR] ----------------------------------------------------------- $NC"
+  log_raw "$PURPLE [HDR] - $1 $NC"
+  log_raw "$PURPLE [HDR] ----------------------------------------------------------- $NC"
 }
 
 function log_detail() {
-  log_raw "$LT_GREEN[DTL]  ====> $1$NC"
+  log_raw "$LT_GREEN [DTL]  ====> $1 $NC"
 }
 
 function log_error() {
-  log_raw "$RED[ERR] $1$NC"
+  log_raw "$RED [ERR] $1 $NC"
 }
 
 function log_warning() {
-  log_raw "$YELLOW[WAR] $1$NC"
+  log_raw "$YELLOW [WAR] $1 $NC"
 }
 
 function log_raw() {
