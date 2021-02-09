d33615bddf620dcbca174f6490264657c7c54043
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-02-09 13:15:26 -0800

    Color Fixes

--- Core/consul/logging-functions.sh
+++ Core/consul/logging-functions.sh
@@ -1,28 +1,28 @@
 #!/bin/sh
 
 function log() {
-  log_raw "${GREEN} [INF] $1 ${NC}"
+  log_raw "${ESCAPE}[${RESET_ALL};${FOREGROUND_GREEN}m [INF] $1 ${ESCAPE}[${RESET_ALL}m"
 }
 
 function log_header() {
   echo " "
-  log_raw "${PURPLE} [HDR] ----------------------------------------------------------- ${NC}"
-  log_raw "${PURPLE} [HDR] - $1 ${NC}"
-  log_raw "${PURPLE} [HDR] ----------------------------------------------------------- ${NC}"
+  log_raw "${ESCAPE}[${RESET_ALL};${FOREGROUND_MAGENTA}m [HDR] ----------------------------------------------------------- ${ESCAPE}[${RESET_ALL}m"
+  log_raw "${ESCAPE}[${RESET_ALL};${FOREGROUND_MAGENTA}m [HDR] - $1 ${ESCAPE}[${RESET_ALL}m"
+  log_raw "${ESCAPE}[${RESET_ALL};${FOREGROUND_MAGENTA}m [HDR] ----------------------------------------------------------- ${ESCAPE}[${RESET_ALL}m"
 }
 
 function log_detail() {
-  log_raw "${LT_GREEN} [DTL]  ====> $1 ${NC}"
+  log_raw "${ESCAPE}[${SET_BOLD};${FOREGROUND_GREEN}m [DTL]  ====> $1 ${ESCAPE}[${RESET_ALL}m"
 }
 
 function log_error() {
-  log_raw "${RED} [ERR] $1 ${NC}"
+  log_raw "${ESCAPE}[${RESET_ALL};${FOREGROUND_RED}m [ERR] $1 ${ESCAPE}[${RESET_ALL}m"
 }
 
 function log_warning() {
-  log_raw "${YELLOW} [WAR] $1 ${NC}"
+  log_raw "${ESCAPE}[${SET_BOLD};${FOREGROUND_YELLOW}m [WAR] $1 ${ESCAPE}[${RESET_ALL}m"
 }
 
 function log_raw() {
-  echo -e "${WHITE} $(date +"%T"): $1"
+  echo -e "${ESCAPE}[${SET_BOLD};${FOREGROUND_LIGHT_GRAY}m $(date +"%T"): $1"
 }
