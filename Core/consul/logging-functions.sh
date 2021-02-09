cb5987402f8a945f8e425a61e1c89242d265307f
Rodney Wimberly <rodney.wimberly@gmail.com>
2021-02-09 13:56:51 -0800

    Colors

--- Core/consul/logging-functions.sh
+++ Core/consul/logging-functions.sh
@@ -1,28 +1,41 @@
 #!/bin/sh
 
+# error - red
+export COLOR_ERROR="${ESCAPE}[${RESET_ALL};${SET_BLINK};${FOREGROUND_RED}m"
+# warning -yellow
+export COLOR_WARNING="${ESCAPE}[${SET_BOLD};;${SET_BLINK};${FOREGROUND_YELLOW}m"
+# information -green
+export COLOR_INFORMATION="${ESCAPE}[${RESET_ALL};${FOREGROUND_GREEN}m"
+# details - light green
+export COLOR_DETAILS="${ESCAPE}[${SET_BOLD};${FOREGROUND_GREEN}m"
+# header - purple
+export COLOR_HEADER="${ESCAPE}[${RESET_ALL};${SET_UNDERLINE};${FOREGROUND_MAGENTA}m"
+# time stamp/entry type - white
+export COLOR_LOG_ENTRY="${ESCAPE}[${SET_BOLD};${FOREGROUND_LIGHT_GRAY}m"
+
 function log() {
-  log_raw "${GREEN}[INF] $1"
+  log_raw "[INF]${COLOR_INFORMATION} $1"
 }
 
 function log_header() {
   echo " "
-  log_raw "${PURPLE}[HDR] -----------------------------------------------------------"
-  log_raw "${PURPLE_UNDERLINE}[HDR] - $1"
-  log_raw "${PURPLE}[HDR] -----------------------------------------------------------"
+  #log_raw "${PURPLE}[HDR] -----------------------------------------------------------"
+  log_raw "[HDR]${COLOR_HEADER} $1"
+  #log_raw "${PURPLE}[HDR] -----------------------------------------------------------"
 }
 
 function log_detail() {
-  log_raw "${LT_GREEN}[DTL]  ====> $1"
+  log_raw "[DTL]${COLOR_DETAILS}  ====> $1"
 }
 
 function log_error() {
-  log_raw "${RED}[ERR] $1"
+  log_raw "[ERR]${COLOR_ERROR} $1"
 }
 
 function log_warning() {
-  log_raw "${YELLOW}[WAR] $1"
+  log_raw "[WAR]${COLOR_WARNING} $1"
 }
 
 function log_raw() {
-  echo -e "${WHITE}$(date +"%T"): $1 ${NC}"
+  echo -e "${COLOR_LOG_ENTRY}$(date +"%T"): $1 ${NC}"
 }
